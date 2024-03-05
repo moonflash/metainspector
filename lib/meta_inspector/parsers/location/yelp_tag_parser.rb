46ad3b957a09e35d77bee38ab0c636d69e6d4b2c
Mojmir Novakovic <mojmir.novakovic@gmail.com>
2024-03-05 16:29:08 +0100

    bugfix

--- lib/meta_inspector/parsers/location/yelp_tag_parser.rb
+++ lib/meta_inspector/parsers/location/yelp_tag_parser.rb
@@ -4,22 +4,22 @@ module MetaInspector
       require 'bigdecimal'
 
       delegate [:pio, :parsed, :base_url]         => :@main_parser
-        
-        def initialize(main_parser) 
+
+        def initialize(main_parser)
           super(main_parser)
           @reg = /([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)/
         end
-        
+
         def location
-          @_locations || parse! 
+          @_locations || parse!
         end
 
         def parse!
-          text = parsed.at('body').inner_text
+          text = parsed.at('body')&.inner_text
 
           @_locations = []
           text.scan(@reg).each do |postcode|
-            loc = pio.lookup(postcode.first) 
+            loc = pio.lookup(postcode.first)
             ap loc.latitude if loc
             @_locations << {:lat => loc.latitude, :lng => loc.longitude, :type => self.class.to_s} if loc
           end
